a8ecdaa8900fc1e70ea2c2de42ead338ea672d49
GizemCimen <80580769+GizemCimen@users.noreply.github.com>
2021-11-14 14:06:10 +0300

    Update hasta_ekrani.java
    hasta_ekranı

--- hasta_ekrani.java
+++ hasta_ekrani.java
@@ -1,4 +1,4 @@
-package com.example.psikolog;
+package com.example.psikolog_gizem.hasta;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -11,12 +11,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.psikolog_gizem.R;
+import com.example.psikolog_gizem.veritabani.VeriTabani;
+import com.example.psikolog_gizem.veritabani.doktorlar;
+import com.example.psikolog_gizem.veritabani.hastalar;
+
+import java.util.ArrayList;
 
 public class hasta_ekrani extends AppCompatActivity {
     private EditText hasta_giris_e_posta;
     private EditText hasta_giris_sifre;
-    private TextView hasta_giris_sifre_unuttum;
+
     private Button hasta_giris_giris_yap_buton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,19 +39,42 @@ public class hasta_ekrani extends AppCompatActivity {
 
         hasta_giris_e_posta=findViewById(R.id.hasta_giris_e_posta);
         hasta_giris_sifre=findViewById(R.id.hasta_giris_sifre);
-        hasta_giris_sifre_unuttum=findViewById(R.id.hasta_giris_sifre_unuttum);
         hasta_giris_giris_yap_buton=findViewById(R.id.hasta_giris_giris_yap_buton);
+
 
         hasta_giris_giris_yap_buton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent giris=new Intent(hasta_ekrani.this, hasta_karsilama_ekrani.class);
-                startActivity(giris);
+                String username=hasta_giris_e_posta.getText().toString();
+                String password=hasta_giris_sifre.getText().toString();
+
+                hastalar hasta=new hastalar();
+                hasta.sethasta_e_posta(username);
+                hasta.sethasta_sifre(password);
+                VeriTabani veriTabani=new VeriTabani(hasta_ekrani.this);
+
+
+                if(username.equals("") || password.equals("")){
+                    Toast.makeText(getApplicationContext(),"ALANLAR BOŞ BIRAKILAMAZ", Toast.LENGTH_LONG).show();
+
+                }
+                else if(!veriTabani.HastaGirisBilgileriniKontrolEt(hasta)){
+                    Toast.makeText(getApplicationContext(),"KULLANICI ADI VEYA ŞİFRE HATALI", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+
+
+                    Intent giris=new Intent(hasta_ekrani.this, hasta_karsilama_ekrani.class);
+                    giris.putExtra("key",hasta_giris_e_posta.getText().toString());
+
+                    startActivity(giris);
+                    finish();
+                }
+
             }
         });
     }
-
-    //BU FONKSİYON BACK TUŞUNU AKTİFLEŞTİRİYOR.
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -50,4 +83,5 @@ public class hasta_ekrani extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
