2715cd398e4d62f7596ec2f7be152d84b6181051
GizemCimen <80580769+GizemCimen@users.noreply.github.com>
2021-11-14 14:16:41 +0300

    Update psikolog_rapor_ekrani.java
    psikolog_rapor_ekranı

--- psikolog_rapor_ekrani.java
+++ psikolog_rapor_ekrani.java
@@ -1,5 +1,7 @@
-package com.example.psikolog.psikolog;
+package com.example.psikolog_gizem.psikolog;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
@@ -11,11 +13,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.psikolog.R;
+import com.example.psikolog_gizem.R;
 
 public class psikolog_rapor_ekrani extends AppCompatActivity {
 
@@ -28,6 +31,11 @@ public class psikolog_rapor_ekrani extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_psikolog_rapor_ekrani);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle("HASTA RAPORU");
 
         kaydet=findViewById(R.id.kaydet);
         rapor_yaz=findViewById(R.id.rapor_yaz);
@@ -46,18 +54,18 @@ public class psikolog_rapor_ekrani extends AppCompatActivity {
         String rapor_metni=rapor_yaz.getText().toString();
         intent.putExtra("rapor",rapor_metni);
         PendingIntent gidilecekIntent=PendingIntent.getActivity(this,1,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            String KanalID="kanal ID";
-            String KanalAd="kanal AD";
-            String KanalTanım="kanal TANIM";
-            int KanalOnceligi=NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel kanal=raporYoneticisi.getNotificationChannel(KanalID);
-            if(kanal==null){
-                kanal=new NotificationChannel(KanalID,KanalAd,KanalOnceligi);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O) {
+            String KanalID = "kanal ID";
+            String KanalAd = "kanal AD";
+            String KanalTanım = "kanal TANIM";
+            int KanalOnceligi = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel kanal = raporYoneticisi.getNotificationChannel(KanalID);
+            if (kanal == null) {
+                kanal = new NotificationChannel(KanalID, KanalAd, KanalOnceligi);
                 kanal.setDescription(KanalTanım);
                 raporYoneticisi.createNotificationChannel(kanal);
             }
-            builder=new NotificationCompat.Builder(this,KanalID);
+            builder = new NotificationCompat.Builder(this, KanalID);
             builder.setContentTitle("BAŞLIK");
             builder.setContentTitle("RAPORUNUZ");
             builder.setSmallIcon(R.drawable.rapor_iconu);
@@ -75,5 +83,14 @@ public class psikolog_rapor_ekrani extends AppCompatActivity {
             builder.setPriority(Notification.PRIORITY_HIGH);
         }
         raporYoneticisi.notify(1,builder.build());
+        finish();
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
