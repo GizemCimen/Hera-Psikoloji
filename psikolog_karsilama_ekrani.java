1ced7766b8a4302f2dc42f23839423a69b43ff4e
GizemCimen <80580769+GizemCimen@users.noreply.github.com>
2021-11-14 14:15:29 +0300

    Update psikolog_karsilama_ekrani.java
    psikolog_listeleme_ekranı

--- psikolog_karsilama_ekrani.java
+++ psikolog_karsilama_ekrani.java
@@ -1,18 +1,17 @@
-package com.example.psikolog.psikolog;
+package com.example.psikolog_gizem.psikolog;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.psikolog.hizmetler.hizmetler_ekrani;
-import com.example.psikolog.MainActivity;
-import com.example.psikolog.R;
+import com.example.psikolog_gizem.R;
 
 public class psikolog_karsilama_ekrani extends AppCompatActivity {
     private Button psikolog_karsilama_ekrani_hasta_goruntuleme;
@@ -23,7 +22,6 @@ public class psikolog_karsilama_ekrani extends AppCompatActivity {
     private Button psikolog_karsilama_ekrani_rapor_kayit;
     private Button psikolog_karsilama_ekrani_bildirimler;
     private Button psikolog_karsilama_ekrani_profil;
-    private Button psikolog_karsilama_ekrani_cikisyap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,61 +31,69 @@ public class psikolog_karsilama_ekrani extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         getSupportActionBar().setTitle("HERA PSİKOLOJİ");
+        String gelen=getIntent().getStringExtra("e_posta");
 
         psikolog_karsilama_ekrani_hasta_goruntuleme=findViewById(R.id.psikolog_karsilama_ekrani_hasta_goruntuleme);
         psikolog_karsilama_ekrani_randevu_goruntuleme=findViewById(R.id.psikolog_karsilama_ekrani_randevu_goruntuleme);
         psikolog_karsilama_ekrani_hizmetler=findViewById(R.id.psikolog_karsilama_ekrani_hizmetler);
-        psikolog_karsilama_ekrani_test=findViewById(R.id.psikolog_karsilama_ekrani_test);
         psikolog_karsilama_ekrani_blog=findViewById(R.id.psikolog_karsilama_ekrani_blog);
         psikolog_karsilama_ekrani_rapor_kayit=findViewById(R.id.psikolog_karsilama_ekrani_rapor_kayit);
         psikolog_karsilama_ekrani_bildirimler=findViewById(R.id.psikolog_karsilama_ekrani_bildirimler);
         psikolog_karsilama_ekrani_profil=findViewById(R.id.psikolog_karsilama_ekrani_profil);
-        psikolog_karsilama_ekrani_cikisyap=findViewById(R.id.psikolog_karsilama_ekrani_cikisyap);
-
-        psikolog_karsilama_ekrani_rapor_kayit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent giris=new Intent(psikolog_karsilama_ekrani.this, psikolog_rapor_ekrani.class);
-            startActivity(giris);
-            }
-        });
-
-        psikolog_karsilama_ekrani_hizmetler.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent giris=new Intent(psikolog_karsilama_ekrani.this, hizmetler_ekrani.class);
-                startActivity(giris);
-            }
-        });
-
-
-
-        psikolog_karsilama_ekrani_bildirimler.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(psikolog_karsilama_ekrani.this, psikolog_bildirim.class);
-                startActivity(intent);
-            }
-        });
 
         psikolog_karsilama_ekrani_blog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent giris=new Intent(psikolog_karsilama_ekrani.this, psikolog_blog.class);
-                startActivity(giris);
+                Uri blog_linki=Uri.parse("https://herapsikoloji.com/blog-sayfasi/");
+                Intent intent =new Intent(Intent.ACTION_VIEW,blog_linki);
+                startActivity(intent);
             }
         });
-
-        psikolog_karsilama_ekrani_cikisyap.setOnClickListener(new View.OnClickListener() {
+        psikolog_karsilama_ekrani_randevu_goruntuleme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent giris=new Intent(psikolog_karsilama_ekrani.this, MainActivity.class);
-                startActivity(giris);
+                Intent intent=new Intent(psikolog_karsilama_ekrani.this,psikolog_randevu_listeleme.class);
+                startActivity(intent);
             }
         });
-
-
+        psikolog_karsilama_ekrani_profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(psikolog_karsilama_ekrani.this,psikolog_profil_sayfasi.class);
+                intent.putExtra("e_posta2",gelen);
+                startActivity(intent);
+            }
+        });
+        psikolog_karsilama_ekrani_hizmetler.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(psikolog_karsilama_ekrani.this,psikolog_hizmetler_sayfasi.class);
+                startActivity(intent);
+            }
+        });
+        psikolog_karsilama_ekrani_hasta_goruntuleme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(psikolog_karsilama_ekrani.this,psikolog_hasta_listeleme_ekrani.class);
+                startActivity(intent);
+            }
+        });
+        psikolog_karsilama_ekrani_rapor_kayit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(psikolog_karsilama_ekrani.this,psikolog_rapor_ekrani.class);
+                startActivity(intent);
+            }
+        });
+        psikolog_karsilama_ekrani_bildirimler.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent giris=new Intent(psikolog_karsilama_ekrani.this, doktor_bildirim.class);
+            startActivity(giris);
+        }
+    });
     }
+
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
